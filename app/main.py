7bb35b6fcef7e7e67470d81e119e8d2aac25a591
LAPTOP-S5RC2PLG\toanh <toanhduc2005@gamil.com>
2026-04-28 20:03:58 +0700

    fix render python version

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:3000,http://localhost:5173,http://localhost:8080").split(","),
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"],
